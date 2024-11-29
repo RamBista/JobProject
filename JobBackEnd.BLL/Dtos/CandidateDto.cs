9edc8957b1b301d03e1fad5f72f41adce728d47d
rambista <rambista027@gmail.com>
2024-11-29 13:01:55 +0545

    added mapper and dto with data annotations

---
+++ JobBackEnd.BLL/Dtos/CandidateDto.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace JobBackEnd.BLL.Dtos;
+
+public class CandidateDto
+{
+    [Required]
+    [MaxLength(100)]
+    public string FirstName { get; set; } = default!;
+
+    [Required]
+    [MaxLength(100)]
+    public string LastName { get; set; } = default!;
+
+    [Phone]
+    [MaxLength(15)]
+    public string PhoneNumber { get; set; } = default!;
+
+    [Required]
+    [EmailAddress]
+    [MaxLength(255)]
+    public string Email { get; set; } = default!;
+
+    [MaxLength(100)]
+    public string? CallTimeInterval { get; set; } = default!;
+
+    [Url]
+    [MaxLength(255)]
+    public string? LinkedInProfileUrl { get; set; } = default!;
+
+    [Url]
+    [MaxLength(255)]
+    public string? GitHubProfileUrl { get; set; } = default!;
+
+    [Required]
+    [MaxLength(1000)]
+    public string Comment { get; set; } = default!;
+}
---
+++ JobBackEnd.BLL/Mappers/CandidateMapper.cs
@@ -0,0 +1,23 @@
+﻿using JobBackEnd.DAL.Context.Entities;
+using JobBackEnd.BLL.Dtos;
+namespace JobBackEnd.BLL.Mappers;
+
+public static class CandidateMapper
+{
+    public static Candidate ToModel(this CandidateDto candidateDto)
+    {
+        return new Candidate()
+        {
+            FirstName = candidateDto.FirstName,
+            LastName = candidateDto.LastName,
+            PhoneNumber = candidateDto.PhoneNumber,
+            Email = candidateDto.Email,
+            CallTimeInterval = candidateDto.CallTimeInterval ?? string.Empty,
+            LinkedInProfileUrl = candidateDto.LinkedInProfileUrl ?? string.Empty,
+            GitHubProfileUrl = candidateDto.GitHubProfileUrl ?? string.Empty,
+            Comment = candidateDto.Comment,
+            CreationDateTime = DateTime.UtcNow.ToString()
+        };
+    }
+
+}
